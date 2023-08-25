f820b9b29e6b1cb13e1abc4261af6764b49c16bb
Ted Osiobe <osiobeted@gmail.com>
2023-08-25 10:54:28 +0000

    Final Correction Made

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
-'''Given a pile of coins of different values,
-    determine the fewest number of coins needed to meet
-    a given amount total.
-'''
-import sys
+""" Making Change Module
+"""
 
 
 def makeChange(coins, total):
-    '''
-    Return: fewest number of coins needed to meet total
-    If total is 0 or less, return 0
-    If total cannot be met by any number of coins you have, return -1
-    '''
+    """ Determines the fewest number of coins needed
+        to meet a given amount `total`.
+
+        Params:
+          * coins - a list of the values of the coins in our possession
+          * total - the total amount of coins needed
+
+        Return: fewest number of coins needed to meet total
+          - If total is 0 or less, return 0
+          - If total cannot be met by any number of coins we have, return -1
+
+        Assumptions:
+          - The value of a coin will always be an integer greater than 0
+          - We have an infinite number of each denomination of coin in the list
+    """
     if total <= 0:
         return 0
-    table = [sys.maxsize for i in range(total + 1)]
-    table[0] = 0
-    m = len(coins)
-    for i in range(1, total + 1):
-        for j in range(m):
-            if coins[j] <= i:
-                subres = table[i - coins[j]]
-                if subres != sys.maxsize and subres + 1 < table[i]:
-                    table[i] = subres + 1
 
-    if table[total] == sys.maxsize:
-        return -1
-    return table[total]
+    coins.sort()
+    coins.reverse()
+    no_of_coins = 0
+    for c in coins:
+        no_of_coins += total // c
+        total = total % c
+        if total == 0:
+            return no_of_coins
+
+    return -1
